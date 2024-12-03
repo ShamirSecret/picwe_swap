e3335258a132e38ea7920c6e25ab15e0befaad34
ShamirSecret <144866944+ShamirSecret@users.noreply.github.com>
2024-12-03 10:00:56 +0800

    "update: build for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/picwe_swap/',
+  base: './',
+  build: {
+    outDir: 'docs',
+  },
   server: {
     proxy: {
       '/v1': {
